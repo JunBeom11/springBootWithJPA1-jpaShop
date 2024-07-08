328b8fdd539e8714ae0c4ccc138c44b517a25895
jun6eom <leejun212@naver.com>
2024-07-08 23:37:26 +0900

    회원 조회v1, v2, @JsonIgnore 추가

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
@@ -24,6 +25,8 @@ public class Member {
     @Embedded
     private Address address;
 
+    // 응답 데이터 Filtering -> response 응답 데이터 제외시키기
+    @JsonIgnore
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
 }
