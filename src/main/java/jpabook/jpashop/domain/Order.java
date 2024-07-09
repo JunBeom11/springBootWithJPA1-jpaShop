9a0d083d0e7a9d145cbc890db7760f34fb95c637
jun6eom <leejun212@naver.com>
2024-07-10 00:06:47 +0900

    API - 엔티티 직접 노출

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -62,8 +62,10 @@ public class Order {
         for (OrderItem orderItem : orderItems) {
             order.addOrderItem(orderItem);
         }
+
         order.setStatus(OrderStatus.ORDER);
         order.setOrderDate(LocalDateTime.now());
+
         return order;
     }
 
